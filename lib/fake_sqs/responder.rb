036f0ab4d8908d36f870890b17e14e2020b74892
glindsay-mdsol <glindsay@mdsol.com>
2014-06-02 10:41:03 -0400

    Added hardcoded 2012-11-05 namespace to responder generated xml.

--- lib/fake_sqs/responder.rb
+++ lib/fake_sqs/responder.rb
@@ -6,7 +6,7 @@ module FakeSQS
 
     def call(name, &block)
       xml = Builder::XmlMarkup.new(:indent => 4)
-      xml.tag! "#{name}Response" do
+      xml.tag!("#{name}Response", "xmlns" => "http://queue.amazonaws.com/doc/2012-11-05/" ) do
         if block
           xml.tag! "#{name}Result" do
             yield xml
