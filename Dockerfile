03b7180267f113849d87ee47d661386c7fea3df1
Donald Arena <darena@mdsol.com>
2015-04-06 17:24:10 -0700

    making it work like a read aws service

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,8 @@
 FROM ruby:2.1.2
 
+RUN apt-get update
+RUN apt-get -y install procps vim
+
 RUN mkdir -p /var/data/fake_sqs
 
 RUN mkdir -p /app
--- docker/start_it_up.sh
+++ docker/start_it_up.sh
@@ -1,2 +1,2 @@
-export MY_ADDR=`/sbin/ip route|awk '/default/ { print $3 }'`
+export MY_ADDR=`getent hosts $HOSTNAME | awk '{print $1}'`
 bin/fake_sqs --database /var/data/fake_sqs/database.yml --bind $MY_ADDR --port 4568
