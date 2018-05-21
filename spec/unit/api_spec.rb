0788a523d341c3d385c941983fca250e6f153e7b
Donald Mckinnon <dmckinnon@mdsol.com>
2018-05-20 20:47:06 -0400

    Fix all specs

--- spec/unit/api_spec.rb
+++ spec/unit/api_spec.rb
@@ -7,21 +7,24 @@ class FakeSQS::Actions::TheAction
   end
 
   def call(params)
-    { :options => @options, :params => params }
+    { options: @options, params: params }
   end
 
 end
 
 
 RSpec.describe FakeSQS::API do
+  before do
+    @queues = double :queues
+  end
 
   it "delegates actions to classes" do
     allow(@queues).to receive(:transaction).and_yield
     api = FakeSQS::API.new(:queues => @queues)
-    response = api.call("TheAction", {:foo => "bar"})
+    response = api.call('TheAction', { foo: 'bar' })
 
-    expect(response[:options]).to eq :queues => queues
-    expect(response[:params]).to eq :foo => "bar"
+    expect(response[:options]).to eq(queues: @queues)
+    expect(response[:params]).to eq(foo: 'bar')
   end
 
   it "raises InvalidAction for unknown actions" do
@@ -32,40 +35,43 @@ RSpec.describe FakeSQS::API do
   end
 
   it "resets queues" do
-    queues = double :queues
-    api = FakeSQS::API.new(:queues => queues)
-    expect(queues).to receive(:reset)
+    api = FakeSQS::API.new(queues: @queues)
+    expect(@queues).to receive(:reset)
     api.reset
   end
 
   it "expires messages in queues" do
-    queues = double :queues
-    api = FakeSQS::API.new(:queues => queues)
-    expect(queues).to receive(:expire)
+    api = FakeSQS::API.new(queues: @queues)
+    expect(@queues).to receive(:expire)
     api.expire
   end
-  
+
   context 'simulates failure' do
     before do
-      @queues.stub(:list).and_return([@queue1=FakeSQS::Queue.new("QueueName" => 'default', :message_factory => MessageFactory.new)])
+      allow(@queues)
+        .to receive(:list)
+        .and_return([
+          @queue1 = FakeSQS::Queue.new(
+            "QueueName" => 'default',
+            message_factory: MessageFactory.new)])
+
       @api = FakeSQS::API.new(:queues => @queues)
       @api.api_fail(:send_message)
       @api.api_fail(:receive_message)
     end
-    
+
     it "fails on sending message" do
       expect { @queue1.send_message }.to raise_error FakeSQS::InvalidAction
     end
-    
+
     it "fails on receiving message" do
       expect { @queue1.receive_message }.to raise_error FakeSQS::InvalidAction
     end
-    
+
     it "resets failures after setting" do
       @api.clear_failure
-      expect { @queue1.send_message }.to_not raise_error 
-      expect { @queue1.receive_message }.to_not raise_error 
+      expect { @queue1.send_message }.to_not raise_error
+      expect { @queue1.receive_message }.to_not raise_error
     end
   end
-
 end
